42e7f14c72b6a5989310cc61f6e439bb67c17b18
Amaraa <amaraa@Amaraas-MacBook-Air.local>
2025-12-23 11:30:04 +0800

    review2

--- script.js
+++ script.js
@@ -15,32 +15,36 @@ const info = [
     img: "./images/beans.jpeg",
     name: "Rowan Atkinson",
     profession: "Actor",
-    description: "mr bean",
+    description:
+      "Rowan Sebastian Atkinson (born 6 January 1955) is an English actor, comedian and writer. He first gained success on the sketch comedy show Not the Nine O'Clock News (1979–1982), before going on to play the title roles in the sitcoms Blackadder (1983–1989) and Mr.",
   },
   {
     img: "./images/bjordan.jpeg",
     name: "Michael B Jordan",
     profession: "Athlete",
-    description: "He is an athlete turned into actor",
+    description:
+      "Jordan (born February 9, 1987, Santa Ana, California, U.S.) is an American actor who parlayed a successful career on television into a series of high-profile movie roles, most notably in the Creed and Black Panther franchises.",
   },
   {
     img: "./images/dweyne.avif",
     name: "Dwayne Johnson",
     profession: "Actor",
-    description: "Famous for being like a rock ",
+    description:
+      "Dwayne Johnson is an actor and former professional wrestler. After turning to wrestling in 1996, he quickly developed into a major star. In the twenty-first century, he increasingly focused on films, becoming one of the highest-grossing actors in the world.",
   },
   {
     img: "./images/tony.jpeg",
     name: "Robert Downey JR",
     profession: "Actor",
-    description: "Iron man ",
+    description:
+      "Robert John Downey Jr. (born April 4, 1965) is an American actor. Known for portraying charismatic and intelligent characters over a diverse range of films, he was the highest-paid actor in Hollywood annually from 2013 to 2015. ",
   },
   {
     img: "./images/will.jpeg",
     name: "Will Smith",
     profession: "Actor",
     description:
-      "He has a crazy range when it comes to acting ,, he is also a rapper in 90s",
+      "Will Smith (born September 25, 1968, Philadelphia, Pennsylvania, U.S.) is an American actor and musician whose charisma and quick wit helped him transition from rap music to a successful career in acting. Smith's notable films include the blockbusters Bad Boys (1995).",
   },
 ];
 
